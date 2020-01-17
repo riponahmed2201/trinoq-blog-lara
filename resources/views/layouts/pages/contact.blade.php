312d5a95d24d0bf0123d2b0c0117dc42c214cc89
riponahmed2201 <riponahmed2201@gmail.com.com>
2020-01-18 01:53:40 +0600

    Error Message showing

--- resources/views/layouts/pages/contact.blade.php
+++ resources/views/layouts/pages/contact.blade.php
@@ -7,15 +7,22 @@
         <div class="col-md-6">
           <h1>Contact Me</h1>
 
-          @if(count($errors) > 0 )
-           <div class="alert alert-danger">
-              <ul>
-                @foreach ($errors as $error)
-                    <li>{{$error}}</li>
+          
+
+        </div>
+      </div>
+      <div class="row blog-entries">
+        <div class="col-md-12 col-lg-8 main-content">
+
+          @if ($errors->any())
+          <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
                 @endforeach
-              </ul>
-           </div>
-          @endif
+            </ul>
+          </div>
+       @endif
 
           @if(\Session::has('success'))
           <div class="alert alert-success">
@@ -23,31 +30,26 @@
          </div>
           @endif
 
-        </div>
-      </div>
-      <div class="row blog-entries">
-        <div class="col-md-12 col-lg-8 main-content">
-          
           <form action="{{route('insertContactData')}}" method="post">
             @csrf 
                 <div class="row">
                   <div class="col-md-12 form-group">
                     <label for="name">Name</label>
-                    <input type="text" id="name" name="name" class="form-control ">
+                    <input type="text" id="name" name="name" class="form-control " value="{{ old('name') }}">
                   </div>
                   <div class="col-md-12 form-group">
                     <label for="phone">Phone</label>
-                    <input type="text" id="phone" name="phone" class="form-control ">
+                    <input type="text" id="phone" name="phone" class="form-control " value="{{ old('phone') }}">
                   </div>
                   <div class="col-md-12 form-group">
                     <label for="email">Email</label>
-                    <input type="email" id="email" name="email" class="form-control ">
+                    <input type="email" id="email" name="email" class="form-control " value="{{ old('email') }}">
                   </div>
                 </div>
                 <div class="row">
                   <div class="col-md-12 form-group">
                     <label for="message">Write Message</label>
-                    <textarea name="message" id="message"  name="message"class="form-control " cols="30" rows="8"></textarea>
+                    <textarea name="message" id="message"  name="message"class="form-control " cols="30" rows="8" value="{{ old('message') }}"></textarea>
                   </div>
                 </div>
                 <div class="row">
